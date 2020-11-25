48505f4f97097007545533dfbf6319a6e8a00e0f
elobdog <ag@broadcom.com>
2020-11-24 18:30:04 -0800

    cleanup, improved os detection logic, etc.

--- unboundblock.sh
+++ unboundblock.sh
@@ -5,14 +5,17 @@ TMPFILE=$(mktemp) || exit 1
 BLOCKL="blockhost.conf"
 UBOUND="/var/unbound"
 DATE=$(date '+%Y%m%d')
+OS=$(uname -s)
+
+trap 'rm -f $TMPFILE' INT HUP TERM
 
 echo "====[script start: $(date)]===="
 
 [ -f $BLOCKL ] && \
 	mv $BLOCKL $BLOCKL.$DATE
 
-if [ "$(uname -s)" == "OpenBSD" ]; then
-	#ftp -S noverifytime -o $TMPFILE $URL
+if [ "$OS" == "OpenBSD" ]; then
+	# use "-S noverifytime" in case of certain errors. man 1 ftp
 	ftp -o $TMPFILE $URL
 else
 	wget -c --passive -O $TMPFILE $URL
@@ -24,9 +27,12 @@ sed -e '/^#/d' -e '/^$/d' \
 	&& mv $TMPFILE.TMP $BLOCKL \
 	&& rm -f $TMPFILE
 
-# Move the file to unbound's directory
-#mv $BLOCKL $UBOUND/etc/ && \
-#	unbound-control reload && \
-	rm -f $BLOCKL.$DATE
+# move the file to unbound's etc directory
+if [ "$OS" == "OpenBSD" ]; then
+	mv $BLOCKL $UBOUND/etc/
+fi
+
+# cleanup, since the new blocklist generated successfully
+rm -f $BLOCKL.$DATE
 
 echo "====[script end: $(date)]===="
